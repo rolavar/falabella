0d461386265197303210027b1e7ac6a5841057a2
Roberto Olavarria <rob.olavarria@gmail.com>
2019-08-15 05:30:14 -0400

    del

--- target/classes/schema/h2-falabella-data.sql
+++
@@ -1,6 +0,0 @@
-insert into BEER(NAME,BREWERY,COUNTRY,PRICE) values('Golden','Kross','Chile',2300);
-insert into BEER(NAME,BREWERY,COUNTRY,PRICE) values('Calafate','Austral','Chile',2000);
-insert into BEER(NAME,BREWERY,COUNTRY,PRICE) values('Arandano','Kunstmann','Chile',1500);
-insert into BEER(NAME,BREWERY,COUNTRY,PRICE) values('Escudo Silver','CCU','Chile',500);
-insert into BEER(NAME,BREWERY,COUNTRY,PRICE) values('Res Ale','Birra Del Borgo','Italia',2600);
-insert into BEER(NAME,BREWERY,COUNTRY,PRICE) values('Budweisser','Anheuser-Busch','Estados Unidos',1700);
--- target/classes/schema/h2-falabella-schema.sql
+++
@@ -1,2 +0,0 @@
-drop table BEER if exists;
-create table BEER (ID integer identity primary key, NAME varchar(255), BREWERY varchar(255), COUNTRY varchar(255), price decimal);
